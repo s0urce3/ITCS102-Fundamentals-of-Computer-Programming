12076b20244a1b1266b0e3af277f9fbe5d5b1947
s0urce3 <vennmarc361@gmail.com>
2026-08-24 10:15:27 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,30 @@
+#using variable
+
+#int
+age1 = 10
+age2 = 12
+age3 = 13
+age4 = 14
+age5 = 15
+
+
+#float
+temp1 = 34.1
+temp2 = 34.4
+temp3 = 34.5
+temp4 = 34.6
+temp5 = 36.5
+
+#string
+username1 = "akolang"
+username2 = "onlyurs"
+username3 = "ikawlang"
+username4 = "mahalko"
+username5 = "part3"
+
+#bolean
+isStudent = False
+
+print("My age is", age1, age2, age3, age4, age5)
+print("My temp is", temp1, temp2, temp3, temp4, temp5)
+print("My string is", username1, username2, username3, username4, username5)
