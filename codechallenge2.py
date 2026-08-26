0918551937c7fca431de7a6d67bb91a2b9e5da76
Venn Marc Gumial <vennmarc361@gmail.com>
2026-08-26 11:32:08 +0800

    Add files via upload

---
+++ codechallenge2.py
@@ -0,0 +1,15 @@
+Deposit = 19863
+
+P1000 = Deposit // 1000
+P500 = Deposit // 500
+P200 = Deposit // 200
+P100 = Deposit // 100
+P50 = Deposit // 50
+P20 = Deposit // 20
+P5 = Deposit // 5
+P1 = Deposit // 1
+
+print("P1000 -",P1000)
+print(
+print(
+print(
