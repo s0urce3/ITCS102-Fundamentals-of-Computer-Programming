a46841babfa5b0008e70ddda3b2b0b5b79fb1a26
Venn Marc Gumial <vennmarc361@gmail.com>
2026-09-04 10:53:15 +0800

    Add files via upload

---
+++ Activity6.py
@@ -0,0 +1,7 @@
+#input (),#type (),eval(),Int(),float()
+
+x = eval(input("Enter a number---> ?"))
+
+print(type(x)) #python function used to determine the data type of variable
+
+#print("x ", Gumial)
