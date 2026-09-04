825f7b3ad61b317c09ea8995b2d4a44a805cff44
Venn Marc Gumial <vennmarc361@gmail.com>
2026-09-04 11:11:28 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,35 @@
+# Operation & Arithmethic & Assignmant symbols
+
+x= 4
+
+x = eval(input("Enter a number--->"))
+x  +  x
+
+b = eval(input("Enter a number--->"))
+x + b
+
+c = eval(input("Enter a number--->"))
+x  + c
+
+d = eval(input("Enter a number--->"))
+x  + d
+
+e = eval(input("Enter a number--->"))
+x  + e
+
+f = eval(input("Enter a number--->"))
+x  - f
+
+g = eval(input("Enter a number--->"))
+x  - g
+
+h = eval(input("Enter a number--->"))
+x  // h
+
+i = eval(input("Enter a number--->"))
+x   *  i
+
+j = eval(input("Enter a number--->"))
+x  * j
+
+print("Current Total is --->", x)
