40a0ff8a5d5178f5c126d97c96cb2dc65eb6dcdc
Venn Marc Gumial <vennmarc361@gmail.com>
2026-09-07 11:04:39 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,17 @@
+#Basic if else program
+
+username = "Venn"
+password = "Marc"
+
+u = input("Input USERNAME -->  ")
+p = input("Input PASSWORD -->  ")
+
+if  u == username :
+	print("username corect")
+else:
+	print("username incorrect")
+
+if  p == password :
+	print("password corect")
+else:
+	print("password incorrect")
