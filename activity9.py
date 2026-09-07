e11520467ffc219ced66da3156a47f4083e0e265
Venn Marc Gumial <vennmarc361@gmail.com>
2026-09-07 10:47:59 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+# Relational Operators
+a = 3
+b = 2
+
+
+password1 = "Gumial"
+password2 = "Gumial"
+
+#1
+print (a < b) 
+#2
+print (a > b) 
+#3
+print (a <= b) 
+#4
+print (a >= b)
+#5 ==
+print (password1 == password2)
+#6 !=
+print (password1 != password2)
